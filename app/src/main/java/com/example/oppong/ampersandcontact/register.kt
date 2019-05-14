52578acadd19f4f4829f7a20c7dd0591e43bfa9a
oppongjr <oppong751@gmail.com>
2019-05-14 21:09:32 +0000

    feat(registration): Registration UI design implemented

---
+++ app/src/main/java/com/example/oppong/ampersandcontact/register.kt
@@ -0,0 +1,12 @@
+package com.example.oppong.ampersandcontact
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
