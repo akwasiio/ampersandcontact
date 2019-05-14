8282c8728a05684afa79b6679908cf112bafc0cf
oppongjr <oppong751@gmail.com>
2019-05-14 21:10:56 +0000

    feat(registration): Registration layout implemented

--- app/src/main/java/com/example/oppong/ampersandcontact/register.kt
+++ app/src/main/java/com/example/oppong/ampersandcontact/register.kt
@@ -3,6 +3,7 @@ package com.example.oppong.ampersandcontact
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
 class register : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
