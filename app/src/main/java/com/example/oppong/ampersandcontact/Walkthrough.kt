1472680f9b7c895b0d7d5bf76eeaec79af8e6f66
oppongjr <oppong751@gmail.com>
2019-05-15 11:05:31 +0000

    feat(walkthrough): Walkthrough layout added. Yet to implement background logic.

---
+++ app/src/main/java/com/example/oppong/ampersandcontact/Walkthrough.kt
@@ -0,0 +1,18 @@
+package com.example.oppong.ampersandcontact
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class Walkthrough : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_walkthrough)
+    }
+
+    fun onRegisterButtonClick(view: View){
+        startActivity(Intent(this, RegisterActivity::class.java))
+    }
+}
