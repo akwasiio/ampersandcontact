ca6cad02f3ee432d3b7b560b20f6b28998f3c9cf
oppongjr <oppong751@gmail.com>
2019-05-17 15:31:04 +0000

    feat: Implemented UI enhancements to Register Screen

--- app/src/main/java/com/example/oppong/ampersandcontact/rest/ApiClient.kt
+++ app/src/main/java/com/example/oppong/ampersandcontact/rest/ApiClient.kt
@@ -1,2 +1,23 @@
 package com.example.oppong.ampersandcontact.rest
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+class ApiClient {
+    companion object {
+        val BASE_URL = "https://ampersand-contact-exchange-api.herokuapp.com/api/v1/"
+        var retrofit: Retrofit? = null
+
+        fun getClient(): Retrofit? {
+            if(retrofit == null){
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(ScalarsConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit
+        }
+    }
+}
--- app/src/main/java/com/example/oppong/ampersandcontact/rest/WebService.kt
+++ app/src/main/java/com/example/oppong/ampersandcontact/rest/WebService.kt
@@ -1,4 +1,24 @@
 package com.example.oppong.ampersandcontact.rest
 
+import com.example.oppong.ampersandcontact.model.User
+import com.example.oppong.ampersandcontact.model.UserAuthResponse
+import retrofit2.Call
+import retrofit2.http.*
+
 interface WebService {
+    @Headers("Content-Type: application/json")
+    @POST("register")
+    fun register(@Body user: User): Call<UserAuthResponse>
+
+    @Headers("Content-Type: application/json")
+    @POST("login")
+    fun login(@Body user: User): Call<UserAuthResponse>
+
+    @Headers(
+        value = ["Content-type: application/json",
+            "x-access-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Yzg2MjNhMzdiNDU3OTAwMD" +
+                    "RhMTZhNjUiLCJpYXQiOjE1NTIyOTQ4ODd9.eMXlcE4e_5N2fSxrQaeYJyGCzBnhL_BeenaroWsaZ9s"]
+    )
+    @GET("profile/{id}")
+    fun getUserProfile(@Path("id") id: String)
 }
