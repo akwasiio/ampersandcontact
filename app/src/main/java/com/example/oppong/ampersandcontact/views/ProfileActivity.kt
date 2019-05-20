19618d9360bc7efd918b6f1db3cce3d0f0bf2809
oppongjr <oppong751@gmail.com>
2019-05-20 03:42:21 +0000

    improvement: Profile page retrieves its data from shared preferences instead of User Object

--- app/src/main/java/com/example/oppong/ampersandcontact/views/ProfileActivity.kt
+++ app/src/main/java/com/example/oppong/ampersandcontact/views/ProfileActivity.kt
@@ -1,9 +1,14 @@
 package com.example.oppong.ampersandcontact.views
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.oppong.ampersandcontact.R
-import com.example.oppong.ampersandcontact.model.User
+import com.example.oppong.ampersandcontact.Utility
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_profile.*
 
@@ -14,18 +19,41 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
 
         val receivedIntent = intent
-        val user = receivedIntent.getSerializableExtra("user") as User
 
-        profileFullNameTextView.text = user.firstName.plus(user.lastName)
-        profileRoleTextView.text = user.role
 
-        Picasso.with(this).load(user.photo).placeholder(R.drawable.ic_user_tb).fit().centerCrop()
+        val prefs = getSharedPreferences("user_details", Context.MODE_PRIVATE)
+        val firstName = prefs.getString("firstName", " ")
+        val lastName = prefs.getString("lastName", " ")
+        val photo = prefs.getString("photo", " ")
+        val email = prefs.getString("email", " ")
+        val phoneNumber = prefs.getString("phone", " ")
+        val role = prefs.getString("role", " ")
+
+        val extraString = receivedIntent.getStringExtra("newHeaderText")
+        if(extraString != null)
+            profileHeaderText.text = extraString
+
+        profileFullNameTextView.text = Utility.makeNameTitleCase(firstName.plus(" ".plus(lastName)))
+        profileRoleTextView.text = role
+
+        Picasso.with(this).load(photo).placeholder(R.drawable.ic_user).fit().centerCrop()
             .into(profileImageCircleView)
 
-        profileMail.text = user.email
-        profileTelephone.text = user.phoneNumber
+        profileMail.text = email
+        profileTelephone.text = phoneNumber
 
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishAffinity()
+        finish()
+        startActivity(Intent(this, HomeQRActivity::class.java))
+    }
+
+    fun onBackArrowPressed(view: View) {
+        onBackPressed()
+    }
 
 }
